ddd2afab2e954971270f1e5d4741424fc24ffbcc
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2023-04-21 01:07:15 +0100

    Removed Unused Declarations

--- src/functions/chessboard/CreateBoard.ts
+++ src/functions/chessboard/CreateBoard.ts
@@ -1,31 +1,31 @@
 function getPiece(char: string) {
-  switch (char){
+  switch (char) {
     case "K":
       return {type:"King", color: "white"};
     case "Q":
-        return {type: "Queen", color: "white"};
+      return {type: "Queen", color: "white"};
     case  "B":
-        return {type: "Bishop", color: "white"};
+      return {type: "Bishop", color: "white"};
     case "N":
-        return {type: "Knight", color: "white"};
+      return {type: "Knight", color: "white"};
     case "R":
-        return {type: "Rook", color: "white"};
+      return {type: "Rook", color: "white"};
     case "P":
-        return {type: "Pawn", color: "white"};
+      return {type: "Pawn", color: "white"};
     case "k":
       return {type:"King", color: "black"};
     case "q":
-        return {type: "Queen", color: "black"};
+      return {type: "Queen", color: "black"};
     case  "b":
-        return {type: "Bishop", color: "black"};
+      return {type: "Bishop", color: "black"};
     case "n":
-        return {type: "Knight", color: "black"};
+      return {type: "Knight", color: "black"};
     case "r":
-        return {type: "Rook", color: "black"};
+      return {type: "Rook", color: "black"};
     case "p":
-        return {type: "Pawn", color: "black"};
+      return {type: "Pawn", color: "black"};
     default:
-        return null;
+      return null;
   };
 };
 
@@ -34,38 +34,34 @@ export function CreateBoard(lightSquareClr: string, darkSquareClr: string): any[
   const Cols = 8;
 
   const startingPosition = [
-      "RNBQKBNR",
-      "PPPPPPPP",
-      "        ",
-      "        ",
-      "        ",
-      "        ",
-      "pppppppp",
-      "rnbqkbne"
+    "RNBQKBNR",
+    "PPPPPPPP",
+    "        ",
+    "        ",
+    "        ",
+    "        ",
+    "pppppppp",
+    "rnbqkbne"
   ];
-
-  const lightSquare = lightSquareClr;
-  const darkSquare = darkSquareClr;
 
   const chessBoard = new Array(Rows);
 
   for(let row = 0; row < Rows; row++){
-      chessBoard[row] = new Array(Cols);
+    chessBoard[row] = new Array(Cols);
 
-      for (let col = 0; col < Cols; col++){
-          const isLightsquare = (row + col) % 2 ===0;
-          const piece = getPiece(startingPosition[row][col]);
-          const color = isLightsquare ? lightSquareClr : darkSquareClr;
+    for (let col = 0; col < Cols; col++){
+      const isLightSquare = (row + col) % 2 ===0;
+      const piece = getPiece(startingPosition[row][col]);
+      const color = isLightSquare ? lightSquareClr : darkSquareClr;
 
-          chessBoard[row][col] = {
-              row,
-              col,
-              color,
-              piece,
-          };
+      chessBoard[row][col] = {
+        row,
+        col,
+        color,
+        piece,
       };
+    };
   };
 
   return chessBoard;
 };
-
