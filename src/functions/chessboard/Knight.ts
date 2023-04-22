7711f9a64f656720dbceb2ba8a2f8ae5d2b6942e
git\JavierS04 <javiersemper509@gmail.com>
2023-04-22 17:46:40 +0100

    knight piece done

--- src/functions/chessboard/Knight.ts
+++ src/functions/chessboard/Knight.ts
@@ -1,13 +1,27 @@
 export function knightPiece(sourceSquare: any, targetSquare: any): boolean {
-  if ((targetSquare.row === sourceSquare.row + 1 || targetSquare.row === sourceSquare.row - 1) && (targetSquare.col === sourceSquare.col + 3 || targetSquare.col + sourceSquare.col - 3))
+  if ((targetSquare.row === sourceSquare.row + 1 && targetSquare.col === sourceSquare.col + 2 ) || (targetSquare.row === sourceSquare.row - 1 && targetSquare.col === sourceSquare.col - 2))
+  {
+    return true;
+    
+  }
+  else if ((targetSquare.col === sourceSquare.col + 1 && targetSquare.row === sourceSquare.row + 2) || (targetSquare.col === sourceSquare.col - 1 && targetSquare.row === sourceSquare.row - 2))
+  {
+    return true;
+  } else if ((targetSquare.row === sourceSquare.row - 1 && targetSquare.col === sourceSquare.col + 2 ) || (targetSquare.row === sourceSquare.row - 2 && targetSquare.col === sourceSquare.col + 1))
+  {
+    return true;
+    
+  }
+  else if ((targetSquare.col === sourceSquare.col - 2 && targetSquare.row === sourceSquare.row + 1) || (targetSquare.col === sourceSquare.col - 1 && targetSquare.row === sourceSquare.row + 2))
   {
     return true;
   }
-  else if ((targetSquare.col === sourceSquare.col + 1 || targetSquare.col === sourceSquare.col - 1) && (targetSquare.row === sourceSquare.row + 3 || targetSquare.row + sourceSquare.row - 3) )
-  {
-    return true;
-  }
+
+
   return false;
+
+
+
   // This function takes in the source square and target square for a move
   // This function needs to validate that this move is valid for this piece
   // Return true if move is valid
