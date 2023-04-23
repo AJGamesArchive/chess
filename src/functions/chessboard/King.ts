cba87bab69bb27739241bf4712755cd31eb11c5a
git\JavierS04 <javiersemper509@gmail.com>
2023-04-23 12:08:38 +0100

    king works as intended

--- src/functions/chessboard/King.ts
+++ src/functions/chessboard/King.ts
@@ -1,4 +1,41 @@
 export function kingPiece(sourceSquare: any, targetSquare: any): boolean {
+  if (sourceSquare.piece.color === targetSquare.piece.color)
+  {
+    return false;
+  }
+  else if (targetSquare.row === sourceSquare.row - 1  && targetSquare.col === sourceSquare.col - 1) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col - 1   && targetSquare.row === sourceSquare.row ) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col - 1 && targetSquare.row === sourceSquare.row + 1) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col  && targetSquare.row === sourceSquare.row - 1) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col  && targetSquare.row === sourceSquare.row + 1) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col + 1 && targetSquare.row === sourceSquare.row - 1) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col + 1 && targetSquare.row === sourceSquare.row ) 
+  {
+    return true;
+  }
+  else if (targetSquare.col === sourceSquare.col + 1 && targetSquare.row === sourceSquare.row + 1) 
+  {
+    return true;
+  }
+
   // This function takes in the source square and target square for a move
   // This function needs to validate that this move is valid for this piece
   // Return true if move is valid
